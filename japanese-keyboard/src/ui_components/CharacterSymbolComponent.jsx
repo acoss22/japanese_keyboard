5faf0bcd967770b1a364fb7ac69c37af16b2d081
acoss22 <anacseq@gmail.com>
2022-08-23 23:00:22 +0100

    feat: add css to list btn

--- japanese-keyboard/src/ui_components/CharacterSymbolComponent.jsx
+++ japanese-keyboard/src/ui_components/CharacterSymbolComponent.jsx
@@ -1,7 +1,8 @@
+import styles from './characterSymbol.module.css';
 
 function CharacterSymbolComponent(props){
     return (
-        <button onClick={() => {props.handleChange(props.char.symbol)}}>
+        <button className={styles.pile} onClick={() => {props.handleChange(props.char.symbol)}}>
             <span>
             {props.char.symbol}
             </span>
