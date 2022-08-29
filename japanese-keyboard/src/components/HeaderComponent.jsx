d24cbf1104dbdbc23c174bd24006a95e99a21c13
andrefnbenevides <andre.rhagnar93@gmail.com>
2022-08-29 22:19:08 +0100

    feat: add header component

---
+++ japanese-keyboard/src/components/HeaderComponent.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import CharacterSymbolComponent from './CharacterSymbolComponent';
+import '../App.css';
+
+function Header(props) {
+    
+    return (
+        <div className="header">
+            Japanese Keyboard by ACOSS
+        </div>
+    );
+}
+
+export default Header;
