e69c2a2fa2d3838ab54f70eb65e88c6736bf25a8
acoss22 <anacseq@gmail.com>
2022-09-08 22:56:42 +0100

    feat: add katakana part 1

--- japanese-keyboard/src/consts/katakana.js
+++ japanese-keyboard/src/consts/katakana.js
@@ -1,4 +1,270 @@
-const katakana= [{
-    symbol: "ア",
-    romanji: "a",
-},];
+const katakana = [
+    {
+        symbol: "ア",
+        romanji: "a",
+    },
+    {
+        symbol: "イ",
+        romanji: "i",
+    },
+    {
+        symbol: "ウ",
+        romanji: "u",
+    },
+    {
+        symbol: "エ",
+        romanji: "e",
+    },
+    {
+        symbol: "オ",
+        romanji: "o",
+    },
+    {
+        symbol: "カ",
+        romanji: "ka",
+    },
+    {
+        symbol: "キ",
+        romanji: "ki",
+    },
+    {
+        symbol: "ク",
+        romanji: "ku",
+    }, {
+        symbol: "ケ",
+        romanji: "ke",
+    }, {
+        symbol: "コ",
+        romanji: "ko",
+    },
+    {
+        symbol: "ガ",
+        romanji: "ga",
+    },
+    {
+        symbol: "ギ",
+        romanji: "gi",
+    },
+    {
+        symbol: "グ",
+        romanji: "gu",
+    }, {
+        symbol: "ゲ",
+        romanji: "ge",
+    }, {
+        symbol: "ゴ",
+        romanji: "go",
+    },
+    {
+        symbol: "サ",
+        romanji: "sa",
+    },
+    {
+        symbol: "シ",
+        romanji: "si",
+    },
+    {
+        symbol: "ス",
+        romanji: "su",
+    }, {
+        symbol: "セ",
+        romanji: "se",
+    }, {
+        symbol: "ソ",
+        romanji: "so",
+    },
+    {
+        symbol: "ザ",
+        romanji: "za",
+    },
+    {
+        symbol: "ジ",
+        romanji: "zi",
+    },
+    {
+        symbol: "ズ",
+        romanji: "zu",
+    }, {
+        symbol: "ゼ",
+        romanji: "ze",
+    }, {
+        symbol: "ゾ",
+        romanji: "zo",
+    },
+    {
+        symbol: "タ",
+        romanji: "ta",
+    },
+    {
+        symbol: "チ",
+        romanji: "ti",
+    },
+    {
+        symbol: "ツ",
+        romanji: "tu",
+    }, {
+        symbol: "テ",
+        romanji: "te",
+    }, {
+        symbol: "ト",
+        romanji: "to",
+    },
+    {
+        symbol: "ダ",
+        romanji: "da",
+    },
+    {
+        symbol: "ヂ",
+        romanji: "di",
+    },
+    {
+        symbol: "ヅ",
+        romanji: "du",
+    }, {
+        symbol: "デ",
+        romanji: "te",
+    }, {
+        symbol: "ド",
+        romanji: "to",
+    },
+    {
+        symbol: "ナ",
+        romanji: "na",
+    },
+    {
+        symbol: "ニ",
+        romanji: "ni",
+    },
+    {
+        symbol: "ヌ",
+        romanji: "nu",
+    }, {
+        symbol: "ネ",
+        romanji: "ne",
+    }, {
+        symbol: "ノ",
+        romanji: "no",
+    },
+    {
+        symbol: "ハ",
+        romanji: "ha",
+    },
+    {
+        symbol: "ヒ",
+        romanji: "hi",
+    },
+    {
+        symbol: "フ",
+        romanji: "hu",
+    }, {
+        symbol: "ヘ",
+        romanji: "he",
+    }, {
+        symbol: "ホ",
+        romanji: "ho",
+    },
+    {
+        symbol: "バ",
+        romanji: "ba",
+    },
+    {
+        symbol: "ビ",
+        romanji: "bi",
+    },
+    {
+        symbol: "ブ",
+        romanji: "bu",
+    }, {
+        symbol: "ベ",
+        romanji: "be",
+    }, {
+        symbol: "ボ",
+        romanji: "bo",
+    },
+    {
+        symbol: "パ",
+        romanji: "pa",
+    },
+    {
+        symbol: "ピ",
+        romanji: "pi",
+    },
+    {
+        symbol: "プ",
+        romanji: "pu",
+    }, {
+        symbol: "ペ",
+        romanji: "pe",
+    }, {
+        symbol: "ポ",
+        romanji: "po",
+    },
+    {
+        symbol: "マ",
+        romanji: "ma",
+    },
+    {
+        symbol: "ミ",
+        romanji: "mi",
+    },
+    {
+        symbol: "ム",
+        romanji: "mu",
+    }, {
+        symbol: "メ",
+        romanji: "me",
+    }, {
+        symbol: "モ",
+        romanji: "mo",
+    },
+    {
+        symbol: "ヤ",
+        romanji: "ya",
+    },
+    {
+        symbol: "ユ",
+        romanji: "yu",
+    },
+    {
+        symbol: "ヨ",
+        romanji: "yu",
+    }, 
+    {
+        symbol: "ラ",
+        romanji: "ra",
+    }, 
+    {
+        symbol: "リ",
+        romanji: "ri",
+    },
+    {
+        symbol: "ル",
+        romanji: "ru",
+    },
+    {
+        symbol: "レ",
+        romanji: "re",
+    },
+    {
+        symbol: "ロ",
+        romanji: "ro",
+    },
+    {
+        symbol: "ワ",
+        romanji: "wa",
+    }, 
+    {
+        symbol: "ヰ",
+        romanji: "wi",
+    },
+    {
+        symbol: "ヱ",
+        romanji: "we",
+    },
+    {
+        symbol: "ヲ",
+        romanji: "wo",
+    },
+
+
+];
