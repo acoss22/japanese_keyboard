b090ad9f43f012ad4a4961fa0ee948f0745b19c0
acoss22 <anacseq@gmail.com>
2023-02-08 11:00:09 +0000

    refactor: index file

---
+++ japanese-keyboard/src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './styles/global.scss';
+import initializeI18n from './i18n';
+
+void initializeI18n().then(() => {
+  ReactDOM.render(
+
+    <React.StrictMode>
+    
+    <App />
+    </React.StrictMode>,
+      document.getElementById('root')
+
+  );
+});
+
+reportWebVitals();
