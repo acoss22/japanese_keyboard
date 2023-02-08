568aadd43ac78e84f5ee62397475a2cbaede1db1
acoss22 <anacseq@gmail.com>
2023-02-08 11:24:42 +0000

    style: fixing toogle padding

--- japanese-keyboard/src/components/header/HeaderComponent.jsx
+++ japanese-keyboard/src/components/header/HeaderComponent.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import styles from './header.module.scss';
-import { useTranslation } from 'react-i18next';
 
 const Header = () => {
-const { t } = useTranslation();
-
     return (
         <div className={styles.wrapper}>
             <div className={styles.header}>
